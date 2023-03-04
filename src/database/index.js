0dbc32954852fd144e22f3a9cd18f1c508ee42fb
pedrocauan <mainyasuo92@gmail.com>
2023-03-04 19:49:05 -0300

    Criando model de validação do usuario

--- src/database/index.js
+++ src/database/index.js
@@ -1,9 +1,10 @@
 import Sequelize from 'sequelize';
 import databaseConfig from '../config/database';
 import Aluno from '../models/Aluno';
+import User from '../models/User';
 
 // Array com todos os models
-const models = [Aluno];
+const models = [Aluno, User];
 
 // Conexão com a database
 const connection = new Sequelize(databaseConfig);
--- src/models/User.js
+++ src/models/User.js
@@ -1,14 +1,60 @@
 import Sequelize, { Model } from 'sequelize';
+import bcryptjs from 'bcryptjs';
 
 export default class User extends Model {
   static init(sequelize) {
     super.init({
-      nome: Sequelize.STRING,
-      email: Sequelize.STRING,
-      password_hash: Sequelize.STRING,
+      nome: {
+        type: Sequelize.STRING,
+        defaultValue: '',
+        /* faz as validações dos campos */
+        validate: {
+          len: {
+            // quantidade de caracteres que deve ter o nome
+            args: [3, 255],
+            // mensagem de erro
+            msg: 'Campo nome deve ter entre 3 e 255 caracteres.',
+          },
+        },
+      },
+      email: {
+        type: Sequelize.STRING,
+        defaultValue: '',
+        unique: {
+          msg: 'Email já existe',
+        },
+
+        /* faz as validações dos campos */
+        validate: {
+          isEmail: {
+            msg: 'Email inválido !!',
+          },
+        },
+      },
+      password_hash: {
+        type: Sequelize.STRING,
+        defaultValue: '',
+      },
+      password: {
+        type: Sequelize.VIRTUAL,
+        defaultValue: '',
+        /* faz as validações dos campos */
+        validate: {
+          len: {
+            args: [6, 50],
+            msg: 'A senha precisa ter entre 6 e 50 caracteres',
+          },
+        },
+      }, /* Campo que nao vai exsitir na base de dados */
     }, {
       sequelize,
     });
+
+    // transforma a senha digitada no formulário em um hash
+    this.addHook('beforeSave', async (user) => {
+      user.password_hash = await bcryptjs.hash(user.password, 8);
+    });
+
     return this;
   }
 }
