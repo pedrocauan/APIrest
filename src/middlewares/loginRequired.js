32c3a8cef41e1d43ddee31ae879fe1124db0cb19
pedrocauan <mainyasuo92@gmail.com>
2023-03-05 18:34:48 -0300

    saindo da sessao qnd o usuario altera o email

--- src/middlewares/loginRequired.js
+++ src/middlewares/loginRequired.js
@@ -1,6 +1,7 @@
 import jwt from 'jsonwebtoken';
+import User from '../models/User';
 
-export default (req, res, next) => {
+export default async (req, res, next) => {
   const { authorization } = req.headers;
 
   // se o token nao for enviado nos headers da requisição ele retorna erro
@@ -17,6 +18,20 @@ export default (req, res, next) => {
     const dados = jwt.verify(token, process.env.TOKEN_SECRET);
 
     const { id, email } = dados;
+
+    const user = await User.findOne({
+      where: {
+        id,
+        email,
+      },
+    });
+
+    if (!user) {
+      return res.status(401).json({
+        errors: ['Usuario inválido'],
+      });
+    }
+
     // coloca id e email na requisição
     req.userId = id;
     req.userEmail = email;
