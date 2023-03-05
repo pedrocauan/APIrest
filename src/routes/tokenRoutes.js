cc3d5ecf20f0b816b203ff85feef16a6048a3c77
pedrocauan <mainyasuo92@gmail.com>
2023-03-05 14:11:30 -0300

    Criando rota para a criação do token

---
+++ src/routes/tokenRoutes.js
@@ -0,0 +1,10 @@
+// === ROTAS DA HOME ===
+
+import { Router } from 'express';
+import tokenController from '../controllers/TokenController';
+
+const router = new Router();
+
+router.post('/', tokenController.store);
+
+export default router;
