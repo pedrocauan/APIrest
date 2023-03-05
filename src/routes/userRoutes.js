a397dc0ee3ae1d3d54caae8dfa00d83e70d3e690
pedrocauan <mainyasuo92@gmail.com>
2023-03-04 22:03:42 -0300

    criando roda put

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -11,6 +11,7 @@ router.post('/', userController.store);
 router.get('/', userController.index);
 
 router.get('/:id', userController.show);
+router.put('/:id', userController.update);
 
 export default router;
 
