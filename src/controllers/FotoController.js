e6d76475d792285231e185d1aa35a7dc97a22aba
pedrocauan <mainyasuo92@gmail.com>
2023-03-09 12:36:52 -0300

    controller de fotos'

--- src/controllers/FotoController.js
+++ src/controllers/FotoController.js
@@ -1,12 +1,13 @@
 import multer from 'multer';
 import multerConfig from '../config/multer';
+import Foto from '../models/Foto';
 
 // single faz com que só possa ser upada uma foto
 const upload = multer(multerConfig).single('foto');
 
 class FotoController {
-  async store(req, res) {
-    return upload(req, res, (error) => {
+  store(req, res) {
+    return upload(req, res, async (error) => {
       // se tiver erro no upload ele da erro
       if (error) {
         return res.status(400).json({
@@ -14,7 +15,14 @@ class FotoController {
         });
       }
 
-      return res.json(req.file);
+      // pega o nome da foto  na requisição
+      const { originalname, filename } = req.file;
+      // pega o id do aluno na requisição
+      const { aluno_id } = req.body;
+      // salva foto na database
+      const foto = await Foto.create({ originalname, filename, aluno_id });
+
+      return res.json(foto);
     });
   }
 }
