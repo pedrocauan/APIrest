68f5e5cc17248c0c45bb68e309ac72342379b8aa
pedrocauan <mainyasuo92@gmail.com>
2023-03-09 12:35:49 -0300

    executando relação 1 p n na database

--- src/database/index.js
+++ src/database/index.js
@@ -2,12 +2,15 @@ import Sequelize from 'sequelize';
 import databaseConfig from '../config/database';
 import Aluno from '../models/Aluno';
 import User from '../models/User';
+import Foto from '../models/Foto';
 
 // Array com todos os models
-const models = [Aluno, User];
+const models = [Aluno, User, Foto];
 
 // Conexão com a database
 const connection = new Sequelize(databaseConfig);
 
 // percorre todos os models e inicia a conexão com a database
 models.forEach((model) => model.init(connection));
+// caso exista a função associate ele executa ela  passando os models dentro como referencia a função
+models.forEach((model) => model.associate && model.associate(connection.models));
